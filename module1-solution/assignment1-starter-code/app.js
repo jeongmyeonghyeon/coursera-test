bd4aee1bea04dc143f9298382cc8d7bcda698a23
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2018-03-25 21:43:21 +0900

    Module 1 Coding Assignment

---
+++ module1-solution/assignment1-starter-code/app.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict';
+
+  angular.module("LunchCheck", [])
+  .controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ["$scope"];
+
+  function LunchCheckController ($scope) {
+    $scope.title = "Lunch Checker";
+    $scope.placeholder = "list comma separated dishes you usually have for lunch";
+    $scope.buttonMessage = "Check If Too Much";
+    $scope.inputColor = "MediumSeaGreen";
+    $scope.inputFood = "";
+    $scope.message = "Please enter data first";
+
+    $scope.lunchCheckLive = function () {
+      var foodLength = $scope.inputFood.split(",").length;
+      if (foodLength > 3) {
+        $scope.inputColor = "Tomato";
+      } else {
+        $scope.inputColor = "MediumSeaGreen";
+      }
+    };
+
+    $scope.lunchCheck = function () {
+      var foodLength = $scope.inputFood.split(",").length;
+      if (foodLength > 3) {
+        $scope.message = "Too much!";
+      } else {
+        $scope.message = "Enjoy!";
+      }
+    };
+  }
+
+})();
